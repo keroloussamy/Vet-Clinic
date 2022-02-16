9b9d071b48bd765340bd188e3e8979ae874f07b1
kerolous samy <keroloussamy98@gmail.com>
2022-02-16 03:27:34 +0200

    Add some queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,44 @@ SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered = true;
 SELECT * from animals WHERE name != 'Gabumon';
 SELECT * from animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * from animals;
+ROLLBACK;
+SELECT * from animals;
+
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon ' WHERE species IS NULL;
+COMMIT;
+SELECT * from animals;
+
+
+BEGIN;
+DELETE FROM animals;
+SELECT * from animals;
+ROLLBACK;
+SELECT * from animals;
+
+
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-1-1';
+SAVEPOINT DeleteAfter_2022;
+UPDATE animals SET weight_kg = weight_kg*-1;
+ROLLBACK TO SAVEPOINT DeleteAfter_2022;
+UPDATE animals SET weight_kg = weight_kg*-1 WHERE weight_kg < 0;
+COMMIT;
+
+
+
+Select count(*) from animals;
+Select count(*) from animals WHERE escape_attempts = 0;
+Select avg(weight_kg) from animals;
+select neutered from animals where escape_attempts = (select max(escape_attempts) from animals);
+SELECt MAX(weight_kg), MIN(weight_kg) from animals GROUP BY species;
+SELECT AVG(escape_attempts) from animals  WHERE date_of_birth between '1990-1-1' AND '2000-1-1' GROUP BY species
