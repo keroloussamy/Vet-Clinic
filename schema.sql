d621e205f2b880484a300cb08b33859abe51ad97
kerolous samy <keroloussamy98@gmail.com>
2022-02-17 01:16:18 +0200

    Add owners and species tables

--- schema.sql
+++ schema.sql
@@ -7,5 +7,19 @@ CREATE TABLE animals (
   escape_attempts INT NOT NULL,
   neutered BOOLEAN NOT NULL,
   weight_kg DECIMAL NOT NULL,
-  species VARCHAR(250)
+  species_id INT NOT NULL,
+  owner_id INT NOT NULL,
+  CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+  CONSTRAINT fk_owner FOREIGN KEY(owner_id) REFERENCES owners(id)
+);
+
+CREATE TABLE owners (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(250) NOT NULL,
+  age INT NOT NULL
+);
+
+CREATE TABLE species (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(250) NOT NULL
 );
