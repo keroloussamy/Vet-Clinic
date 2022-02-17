ff394643b2a352b7025cad8f9194e43af12f9c45
kerolous samy <keroloussamy98@gmail.com>
2022-02-17 22:44:01 +0200

    Insert some data

--- data.sql
+++ data.sql
@@ -47,3 +47,42 @@ WHERE name like '%mon';
 UPDATE animals
 SET species_id = (SELECT id from species WHERE name = 'Pokemon')
 WHERE species_id IS NULL;
+
+
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES 
+  ('William Tatcher', 45, '2000-04-23'),
+  ('Maisy Smith', 26, '2019-01-17'),
+  ('Stephanie Mendez', 64, '2981-05-4'),
+  ('Jack Harkness', 38, '2008-06-8')
+
+
+INSERT INTO specializations (vets_id, species_id)
+VALUES 
+  (1, 1),
+  (3, 1),
+  (3, 2),
+  (4, 2)
+
+INSERT INTO visits (vets_id, animals_id, date)
+VALUES 
+  (4, 1, '2020-05-24'),
+  (4, 3, '2020-1-22'),
+  (8, 4, '2021-2-2'),
+  (9, 2, '2020-1-5'),
+  (9, 2, '2020-3-8'),
+  (9, 2, '2020-5-14'),
+  (10, 3, '2021-5-4'),
+  (1, 4, '2021-2-24'),
+  (2, 2, '2019-12-21'),
+  (2, 1, '2020-8-10'),
+  (2, 2, '2021-4-7'),
+  (3, 3, '2019-9-29'),
+  (4, 4, '2020-10-3'),
+  (4, 4, '2020-11-4'),
+  (5, 2, '2019-1-24'),
+  (5, 2, '2019-5-15'),
+  (5, 2, '2020-2-27'),
+  (5, 2, '2020-8-3'),
+  (5, 3, '2020-5-24'),
+  (5, 1, '2021-1-11')
