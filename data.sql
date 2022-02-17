9ab3bf6fc04bf43956209e885a05840bee1b366f
kerolous samy <keroloussamy98@gmail.com>
2022-02-17 02:46:56 +0200

    Update inseart data

--- data.sql
+++ data.sql
@@ -17,13 +17,33 @@ VALUES
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species_id, owner_id)
 VALUES 
-  ('Charmander', '2020-02-08', 0, false, -11, 1, 4),
-	('Plantmon', '2022-11-15', 2, true, -5.7, 2, 3),
-	('Squirtle', '1993-04-02', 3, false, -12.13, 1, 4),
-  ('Angemon', '2005-06-12', 1, true, -45, 2, 1),
-  ('Boarmon', '2005-06-07', 7, true, 20.4, 2, 5),
-  ('Blossom', '1998-08-13', 3, true, 17, 1, 4),
-  ('Agumon', '2020-2-3', 0, true, 10.23, 2, 5),
-  ('Gabumon', '2018-11-15', 2, true, 8, 2, 2),
-  ('Pikachu', '2021-1-7', 1, false, 15.04, 1, 2),
-  ('Devimon', '2017-5-12', 5, true, 11, 2, 3)
+  ('Charmander', '2020-02-08', 0, false, -11),
+	('Plantmon', '2022-11-15', 2, true, -5.7),
+	('Squirtle', '1993-04-02', 3, false, -12.13),
+  ('Angemon', '2005-06-12', 1, true, -45),
+  ('Boarmon', '2005-06-07', 7, true, 20.4),
+  ('Blossom', '1998-08-13', 3, true, 17),
+  ('Agumon', '2020-2-3', 0, true, 10.23),
+  ('Gabumon', '2018-11-15', 2, true, 8),
+  ('Pikachu', '2021-1-7', 1, false, 15.04),
+  ('Devimon', '2017-5-12', 5, true, 11)
+
+
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander';
+UPDATE animals SET owner_id = 3 WHERE name = 'Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name = 'Squirtle';
+UPDATE animals SET owner_id = 1 WHERE name = 'Angemon';
+UPDATE animals SET owner_id = 5 WHERE name = 'Boarmon';
+UPDATE animals SET owner_id = 4 WHERE name = 'Blossom';
+UPDATE animals SET owner_id = 5 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon';
+
+UPDATE animals
+SET species_id = (SELECT id from species WHERE name = 'Digimon')
+WHERE name like '%mon';
+
+UPDATE animals
+SET species_id = (SELECT id from species WHERE name = 'Pokemon')
+WHERE species_id IS NULL;
